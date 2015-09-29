a0d986896ccc2391e32ba7557f0d990f74f3490c
Katie Byers <lobsterkatie@gmail.com>
2015-09-29 01:21:15 -0700

    color and weight added

--- melon_info.py
+++ melon_info.py
@@ -49,15 +49,5 @@ def print_melon_data(melon_names, melon_data):
         print "\n" + melon_name.upper() + ":"
         print melon_data[melon_name]
 
-# def print_melon(name, seedless, price):
-#     have_or_have_not = 'have'
-#     if seedless:
-#         have_or_have_not = 'do not have'
-
-#     print "{}s {} seeds and are ${:.2f}".format(name, have_or_have_not, price)
-
-
-# for i in melon_names:
-#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
 
 main()
